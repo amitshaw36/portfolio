051c9096afb065199e0b9d7214ef608c9865e4d9
Amit Shaw <amitshawofficial36@gmail.com>
2025-09-04 18:49:24 +0530

    Update TiltText.jsx

--- src/components/TiltText.jsx
+++ src/components/TiltText.jsx
@@ -1,17 +1,24 @@
-
-import React, { forwardRef } from 'react';
+import React, { forwardRef } from "react";
 
 const TiltText = forwardRef((props, ref) => {
   return (
-    <div ref={ref} className='text-right text-white font-serif will-change-transform'>
-      <h1 className='text-[5vw] text-black sm:text-4xl md:text-4xl lg:text-5xl uppercase font-bold leading-tight'>
-        I'm <span className='text-black'>the</span>
+    <div
+      ref={ref}
+      className="text-center md:text-right text-white font-serif will-change-transform px-4"
+    >
+      {/* Line 1 */}
+      <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl uppercase font-bold leading-tight">
+        I'm <span className="text-black">the</span>
       </h1>
-      <h1 className='text-[4vw] text-black sm:text-2xl md:text-3xl lg:text-4xl uppercase font-semibold'>
-        <span className='text-black font-extrabold'>Machine</span> Learning
+
+      {/* Line 2 */}
+      <h1 className="text-2xl text-black sm:text-3xl md:text-4xl lg:text-5xl uppercase font-semibold">
+        <span className="text-black font-extrabold">Machine</span> Learning
       </h1>
-      <h1 className='text-[3.5vw] sm:text-xl md:text-2xl lg:text-3xl uppercase font-medium'>
-        with <span className='text-black font-extrabold'>Engineer</span>
+
+      {/* Line 3 */}
+      <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl uppercase font-medium">
+        with <span className="text-black font-extrabold">Engineer</span>
       </h1>
     </div>
   );
