b60be72ad7e587ff7c94c789f5629913e0166dd7
Amit Shaw <amitshawofficial36@gmail.com>
2025-09-06 00:55:09 +0530

    Update Page1.jsx

--- src/pages/Page1.jsx
+++ src/pages/Page1.jsx
@@ -36,7 +36,7 @@ const Page1 = () => {
   }, []);
 
   return (
-    <div ref={containerRef} className="w-full min-h-screen bg-white overflow-hidden">
+    <div ref={containerRef} className="min-h-screen bg-white p-4 sm:p-7">
 
       <div className="relative h-full w-full shadow-xl shadow-gray-600 rounded-3xl overflow-hidden">
         {/* Responsive Background Image */}
